9f1ff076acf9d2d9baf795c06b636b66bc3f6895
Channel Manager Pro <75109904+channel-manager-pro@users.noreply.github.com>
2020-11-28 13:09:11 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -30,7 +30,7 @@ class Messages():
         
         "**Commmands**\n__/ForceSubscribe - To get the current settings.\n/ForceSubscribe no/off/disable - To turn of ForceSubscribe.\n/ForceSubscribe {channel username} - To turn on and setup the channel.\n/ForceSubscribe clear - To unmute all members who muted by me.\n\nNote: /FSub is an alias of /ForceSubscribe__",
         
-        "**Developed by @Kaveesha_Induwara**"
+        "**Developed by @Kaveesha_Induwara and @InukaASiTH**"
       ]
 
       START_MSG = "**Hey [{}](tg://user?id={})**\n__I can force members to join a specific channel before writing messages in the group.\nLearn more at /help__"
